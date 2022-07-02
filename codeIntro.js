a43763ee96bc5cf0d39e19e7e7b06d437605e537
Benjamin rosenthal <benrrosenthal@gmail.com>
2022-07-02 15:38:46 -0600

    Added readLine.js for assessment and updated codeIntro.js from lecture notes

--- codeIntro.js
+++ codeIntro.js
@@ -1,1 +1,35 @@
 //Practice for intro
+
+const firstName = 'Benjamin'
+let lastName = 'Rosenthal'
+var age = 24
+
+// console.log(firstName, lastName);
+let weight;
+// console.log(weight);
+
+weight = 999;
+// console.log(weight);
+
+
+let x=4;
+let y=5;
+let z=6;
+
+const sum = x+y
+// console.log(sum);
+
+const product = y*z
+// console.log(product)
+
+const newProduct = sum + product
+// console.log(newProduct)
+
+const string1 = "1"
+const string2 = "two"
+const string3 = "3 + 3"
+
+// console.log(string1 + string2);
+
+
+
